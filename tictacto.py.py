03ef507dfffb506b5a5d0f74ecc69a7fa4b9a47a
Nanda Kishore <116256681+nandu1331@users.noreply.github.com>
2023-11-29 19:58:28 +0530

    Add files via upload

---
+++ tictacto.py.py
@@ -0,0 +1,105 @@
+import math
+import random
+
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+    print()
+
+def is_winner(board, player):
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or \
+           all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or \
+       all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_board_full(board):
+    return all(board[i][j] != ' ' for i in range(3) for j in range(3))
+
+def get_empty_cells(board):
+    return [(i, j) for i in range(3) for j in range(3) if board[i][j] == ' ']
+
+def minimax(board, depth, maximizing_player, alpha, beta):
+    if is_winner(board, 'X'):
+        return -1
+    elif is_winner(board, 'O'):
+        return 1
+    elif is_board_full(board):
+        return 0
+
+    if maximizing_player:
+        max_eval = -math.inf
+        for i, j in get_empty_cells(board):
+            board[i][j] = 'O'
+            eval = minimax(board, depth + 1, False, alpha, beta)
+            board[i][j] = ' '
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval
+    else:
+        min_eval = math.inf
+        for i, j in get_empty_cells(board):
+            board[i][j] = 'X'
+            eval = minimax(board, depth + 1, True, alpha, beta)
+            board[i][j] = ' '
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval
+
+def get_best_move(board):
+    best_val = -math.inf
+    best_move = None
+    for i, j in get_empty_cells(board):
+        board[i][j] = 'O'
+        move_val = minimax(board, 0, False, -math.inf, math.inf)
+        board[i][j] = ' '
+        if move_val > best_val:
+            best_move = (i, j)
+            best_val = move_val
+    return best_move
+
+def main():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    player_turn = random.choice([True, False])
+
+    print("Welcome to Tic-Tac-Toe!")
+    print_board(board)
+
+    while True:
+        if player_turn:
+            row = int(input("Enter the row (0, 1, or 2): "))
+            col = int(input("Enter the column (0, 1, or 2): "))
+            if board[row][col] == ' ':
+                board[row][col] = 'X'
+                player_turn = not player_turn
+            else:
+                print("Cell already taken. Try again.")
+                continue
+        else:
+            print("AI is making a move...")
+            move = get_best_move(board)
+            board[move[0]][move[1]] = 'O'
+            player_turn = not player_turn
+
+        print_board(board)
+
+        if is_winner(board, 'X'):
+            print("You win!")
+            break
+        elif is_winner(board, 'O'):
+            print("AI wins!")
+            break
+        elif is_board_full(board):
+            print("It's a draw!")
+            break
+
+if __name__ == "__main__":
+    main()
